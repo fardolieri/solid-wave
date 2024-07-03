088de9389a817cb65c07c4e0c3d565e56ed39bd9
Daniel Mittereder <danielmittereder@gmail.com>
2024-07-03 18:15:56 +0200

    Init shadcn

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,18 @@
-import { defineConfig } from "@solidjs/start/config"
+import { defineConfig } from '@solidjs/start/config'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   ssr: false,
+
+  vite: {
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'), // alias for shadcn
+      },
+    },
+  },
 })
